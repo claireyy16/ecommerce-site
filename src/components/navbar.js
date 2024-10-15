61703243fd752d4d93a3f589b9313374924a7fbc
Claire Cui <clairecui16@gmail.com>
2024-10-15 19:47:38 -0400

    hello

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,14 @@
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { cartcontext } from '../cartcontext';
 
 function NavBarComponent () {
+    const cart = useContext(cartcontext);
     const [ show, setShow ] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
 
     return (
         <>
@@ -13,14 +16,28 @@ function NavBarComponent () {
         <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse className='justify-content-end'>
-            <Button onClick={handleShow}>Cart 0</Button>
+            <Button onClick={handleShow}>Cart: ({productsCount} Items)</Button>
         </Navbar.Collapse>
         </Navbar>
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Shopping Cart</Modal.Title>
                 <Modal.Body>
-                    <h3>body!</h3>
+                    {productsCount > 0 ? 
+                        <>
+                            <p>Items in your cart!</p>
+                            {cart.items.map((currentProduct, idx) => (
+                                <h1>{currentProduct.id}</h1>
+                            ))}
+
+                            <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+                            <Button variant="success"> 
+                                Purchase items!
+                            </Button>
+                        </>
+                    :
+                        <h1>no items!</h1>
+                    }
                 </Modal.Body>
             </Modal.Header>
         </Modal>
--- src/components/productCard.js
+++ src/components/productCard.js
@@ -14,8 +14,22 @@ function ProductCard(props) {
             <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                {/**<Card.Img as={Image} src={"../assets/test.jpg"} alt={"product text"}></Card.Img>**/}
+                <Card.Img as={Image} src={"../assets/test.jpg"} alt={"product text"}></Card.Img>
+                <div></div>
+                { productQuantity > 0 ?
+                <>
+                    <Form as={Row}>
+                        <Form.Label column="true" sm="6">In cart: {productQuantity}</Form.Label>
+                        <Col sm="6">
+                                <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="mx-2">+</Button>
+                                <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className="mx-2">-</Button>
+                            </Col>
+                    </Form>
+                    <Button variant="danger" onClick={() => cart.deleteFromCart(product.id)} className="mx-2">Remove from cart</Button>
+                </>
+                :
                 <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>Add to cart</Button>
+                }
             </Card.Body>
         </Card>
     )
